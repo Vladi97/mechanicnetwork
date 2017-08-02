e54326f274607c30fd6cba70845d7b842f593067
Vladi97 <vmatarrita88@gmail.com>
2017-08-02 01:45:08 -0600

    methods
    creación de métodos para validar usuario y buscar archivos listos

--- account/application.js
+++ account/application.js
@@ -16,6 +16,21 @@ $(function() {
 	});
 });
 
+//Log In
+$(function() {
+    $('#login-link').click(function(e) {
+      debugger;
+      var email = document.getElementById("lg_username").value;
+      var prd = document.getElementById("lg_password").value;
+      if (email=="admin") {
+        if (prd=="admin") {
+            window.location = "/mechanicnetwork/account/profile.html";
+        }
+      }
+    e.preventDefault();
+  });
+});
+
 //profile
 
 $(function () {
@@ -25,3 +40,32 @@ $(function () {
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
+
+  /*file manager*/
+
+  'use strict';
+
+;( function( $, window, document, undefined )
+{
+  $( '.inputfile' ).each( function()
+  {
+    var $input   = $( this ),
+      $label   = $input.next( 'label' ),
+      labelVal = $label.html();
+
+    $input.on( 'change', function( e )
+    {
+      var fileName = '';
+
+      if( this.files && this.files.length > 1 )
+        fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+      else if( e.target.value )
+        fileName = e.target.value.split( '\\' ).pop();
+
+      if( fileName )
+        $label.find( 'span' ).html( fileName );
+      else
+        $label.html( labelVal );
+    });
+  });
+})( jQuery, window, document );
