1d1859e6e86f57f2878a5c61275bba61647c38e8
Vladi97 <vmatarrita88@gmail.com>
2017-08-19 19:15:00 -0600

    Login
    Login funcionando con javascript

--- js/application.js
+++ js/application.js
@@ -1,36 +1,3 @@
-//registro
-$(function() {
-    $('#login-form-link').click(function(e) {
-    $("#login-form").delay(100).fadeIn(100);
-    $("#register-form").fadeOut(100);
-    $('#register-form-link').removeClass('active');
-    $(this).addClass('active');
-    e.preventDefault();
-  });
-  $('#register-form-link').click(function(e) {
-    $("#register-form").delay(100).fadeIn(100);
-    $("#login-form").fadeOut(100);
-    $('#login-form-link').removeClass('active');
-    $(this).addClass('active');
-    e.preventDefault();
-  });
-});
-
-//Log In
-$(function() {
-    $('#login-link').click(function(e) {
-      debugger;
-      var email = document.getElementById("lg_username").value;
-      var prd = document.getElementById("lg_password").value;
-      if (email=="admin") {
-        if (prd=="admin") {
-            window.location = "/mechanicnetwork/account/profile.html";
-        }
-      }
-    e.preventDefault();
-  });
-});
-
 //profile
 
 $(function () {
--- js/login.js
+++ js/login.js
@@ -1,6 +1,30 @@
 document.getElementById("forget_button").addEventListener("click", forget);
+document.getElementById("login_button").addEventListener("click", go);
+	
+var users = JSON.parse(localStorage.getItem('users'));
 
 function forget(){
+	for (var i = 0; i < users.length; i++) {
+		var u = users[i];
+		var email=document.getElementById("emailname_login").value;
+		if (u.email==email) {
+			alert("Hola "+u.name+"!"+"\nSu contraseña es "+ u.password);
+		}
+	}
+}
+
+
+function go(){
+	var user = document.getElementById("emailname_login").value;
+	var pass = document.getElementById("password_login").value;
 	debugger;
-	document.getElementById("form2").style = "display: block;"
+	for (var i = 0; i < users.length; i++) {
+		var u = users[i];
+		if (u.email==user) {
+			if (u.password==pass) {
+                localStorage.setItem('current_user', JSON.stringify(u));
+                window.location= "index.html";
+			}
+		}
+	}
 }
