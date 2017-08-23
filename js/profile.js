926a6e80f3980560461ca57b7c8da9ec6dd525fb
Vladi97 <vmatarrita88@gmail.com>
2017-08-23 13:45:58 -0600

    html
    eliminando partes del html

--- js/profile.js
+++ js/profile.js
@@ -11,11 +11,10 @@ $(function () {
 
 $( document ).ready(function() {
 	load;
-	document.getElementById("show_name").text = "Hola " + current_user.name + "!";
-
 });
 
 function load(){
+	debugger;
 	var current_user = JSON.parse(localStorage.getItem('current_user'));
 
 	if (!current_user) {
@@ -32,5 +31,6 @@ function load(){
 		"<br><label>Dueño: <span contenteditable=\"true\">"+ current_user.dueno+"</span></label><br><label>Correo: <span"+
 		"contenteditable=\"true\">"+current_user.email+"</span></label>";
 	}
+	document.getElementById("show_name").text = current_user.name + current_user.lastname;
 	document.getElementById("join").inerHTML = html;
 }
