3cff1ab1d7f367c1a74858302967e52eb821e87d
Vladi97 <vmatarrita88@gmail.com>
2017-08-23 00:45:16 -0600

    Search
    Método para buscar por nombre listo

--- js/account_index.js
+++ js/account_index.js
@@ -1,4 +1,5 @@
 var current_user = JSON.parse(localStorage.getItem('current_user'));
+var current_result = [];
 
 if (!current_user) {
 	current_user=[];
@@ -26,9 +27,9 @@ function getWorkShops(){
 		html += "Taller de mécanica en excelentes condiciones para atender cualquier problema que se le presente, " + w.dueno +" es el dueño de "+
 		"este fabuloso taller";
 		html += "</p></div><footer class=\"w3-container w3-blue\"><h5>Ver</h5></footer>";
-		products[i]
 	}
 	document.getElementById("contenedor").innerHTML = html;
+	current_result = "workshops";
 }
 
 document.getElementById("search_products").addEventListener("click", getProducts);
@@ -47,7 +48,51 @@ function getProducts(){
 		html += p.name + "</h1></header><div class=\"w3-container\"><p>";
 		html += "Marca: " + p.mark + "\n" + "Vendedor: " +p.dueno + "\n" + "Precio: " + p.precio;
 		html += "</p></div><footer class=\"w3-container w3-blue\"><h5>Ver</h5></footer>";
-		products[i]
+	}
+	document.getElementById("contenedor").innerHTML = html;
+	current_result = "products";
+}
+
+document.getElementById("search_button").addEventListener("click", searchByName);
+
+function searchByName(){
+	var name = document.getElementById("search").value;
+	String html = "";
+	if (current_result=="workshops") {
+		var products = JSON.parse(localStorage.getItem('products'));
+
+		for (var i = 0; i < products.length; i++) {
+			p = products[i];
+
+			if (!products) {
+				products=[];
+			}
+
+			if (p.name == name) {{
+				html += "<div class=\"card\"><header class=\"w3-container w3-blue\"><h1>";
+				html += p.name + "</h1></header><div class=\"w3-container\"><p>";
+				html += "Marca: " + p.mark + "\n" + "Vendedor: " +p.dueno + "\n" + "Precio: " + p.precio;
+				html += "</p></div><footer class=\"w3-container w3-blue\"><h5>Ver</h5></footer>";
+			}
+		}
+	}else{
+		var workshops = JSON.parse(localStorage.getItem('workshops'));
+
+		if (!workshops) {
+			workshops=[];
+		}
+
+		for (var i = 0; i < workshops.length; i++) {
+			var w = workshops[i];
+
+			if (name==w.name) {
+				html += "<div class=\"card\"><header class=\"w3-container w3-blue\"><h1>";
+				html += w.mark + "</h1></header><div class=\"w3-container\"><p>";
+				html += "Taller de mécanica en excelentes condiciones para atender cualquier problema que se le presente, " + w.dueno +" es el dueño de "+
+				"este fabuloso taller";
+				html += "</p></div><footer class=\"w3-container w3-blue\"><h5>Ver</h5></footer>";
+			}
+		}
 	}
 	document.getElementById("contenedor").innerHTML = html;
 }
